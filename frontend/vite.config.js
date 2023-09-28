e73e036ac3343832c3db86bcd3dd278819f365e6
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2023-09-28 11:10:25 +0530

    3rd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
