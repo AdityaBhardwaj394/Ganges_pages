6c0266f267d49843111876e573309d56bd1849bf
Aditya bhardwaj <bhardwajaditya394@gmail.com>
2023-09-28 11:08:02 +0530

    3rd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// vite.config.js
-export  default {
+export default defineConfig({
   build: {
+    // generate manifest.json in outDir
+    manifest: true,
     rollupOptions: {
-      output: {
-        format: 'es', // or 'module' for ES modules
-      },
+      // overwrite default .html entry
+      input: '/src/main.jsx',
     },
   },
-};
+})
